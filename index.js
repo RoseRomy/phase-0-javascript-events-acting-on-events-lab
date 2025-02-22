5f51948336117f4f6b3e8b5d74907ef1e6e6afcd
+ <RoseKamau@Romys-Macboook-Air.local>
2025-02-22 23:46:22 +0300

    Addes index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.left = dodger.style.left || "0px";
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight () {
+    let left = parseInt(dodger.style.left.replace("px", ""), 10);
+    if (!isNaN(left) && left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
